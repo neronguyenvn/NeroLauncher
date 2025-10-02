37ba287b54431e107a4985650f45b441fb89a3ae
neronguyenvn <neronguyenvn@gmail.com>
2025-10-02 17:17:45 +0700

    DEV: bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.neronguyenvn.nerolauncher"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.neronguyenvn.nerolauncher"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 2
         versionName = "0.1.1"
     }
